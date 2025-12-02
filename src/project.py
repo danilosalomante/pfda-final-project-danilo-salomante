6e3548f9ddbc89e3b517adafee3159fb94fbc570
Danilo Salomante <junjunsalomante@gmail.com>
2025-12-01 21:43:36 -0600

    Outlined functions for development.

--- src/project.py
+++ src/project.py
@@ -7,3 +7,13 @@ def main():
     The main function that calls other functions to perform sprite conversion.
     
     '''
+def image_to_pixels(input_path, sprite_size, pallete = None):
+    '''
+    Converts image to sprite with optional pallete.
+    
+    '''
+def run_ui():
+    '''
+    Main application UI.
+    
+    '''
