d0ca759c89bdc283cc6a0d852cb59a59285d756f
Danilo Salomante <junjunsalomante@gmail.com>
2025-12-07 22:16:38 -0600

    Added "128x128" size option.

--- src/project.py
+++ src/project.py
@@ -36,7 +36,7 @@ def run_ui():
     root.geometry("400x250")
 
     # Sprite size selection
-    sprite_sizes = ["8x8", "16x16", "32x32", "64x64"]
+    sprite_sizes = ["8x8", "16x16", "32x32", "64x64", "128x128"]
     sprite_size_var = StringVar(root)
     sprite_size_var.set(sprite_sizes[0])
 
